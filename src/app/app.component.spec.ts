7a371d86e5861967b8037998b23efe4224938061
Kaliane Viesseli <kally_lari@hotmail.com>
2023-11-16 21:13:59 -0300

    ajuste nome projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular--app'`, () => {
+  it(`should have as title 'caderno-de-receitas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular--app');
+    expect(app.title).toEqual('caderno-de-receitas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular--app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('caderno-de-receitas app is running!');
   });
 });
